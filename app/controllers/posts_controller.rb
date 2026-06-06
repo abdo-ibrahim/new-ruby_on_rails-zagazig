3b507251ad3e3e931dcc0551311be6e2bf77821f
abdo <abdo.ibrahim411@gmail.com>
2026-06-06 14:35:54 +0300

    Add PostsController with full CRUD actions

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,3 +1,53 @@
 class PostsController < ApplicationController
-  # your code goes here
+  # GET /posts
+  def index
+    @posts = Post.all
+  end
+
+  # GET /posts/:id
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  # GET /posts/new
+  def new
+    @post = Post.new
+  end
+
+  # POST /posts/commit
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to @post, notice: 'Post was successfully created.'
+    else
+      render :new
+    end
+  end
+
+    # GET /posts/:id/edit
+    def edit
+      @post = Post.find(params[:id])
+    end
+
+    # PATCH/PUT /posts/:id
+    def update
+      @post = Post.find(params[:id])
+      if @post.update(post_params)
+        redirect_to @post, notice: 'Post was successfully updated.'
+      else
+        render :edit
+      end
+    end
+
+    # DELETE /posts/:id
+    def destroy
+      @post = Post.find(params[:id])
+      @post.destroy
+      redirect_to posts_path, notice: 'Post was successfully destroyed.'
+    end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :content)
+  end
 end
